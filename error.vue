5f7a211223ed3cb8d42028f671091eff877809c5
Filipp Romanov <59692476+Flk-dev@users.noreply.github.com>
2025-09-06 15:57:00 +0500

    Output error

--- error.vue
+++ error.vue
@@ -3,6 +3,7 @@
     <Top />
     <Header />
       <div class="error__page">
+        {{ error }}
         <div class="error__page-container container">
           <NuxtLayout name="default" v-if="error.statusCode !== 404">
             <!-- Content for general errors (e.g., 500) -->
